e0ea976847a275a577fd0f2453d806fc8268c06d
AntonNevmerzhitskiy <anevmerzhickiy@pyrus.com>
2020-06-05 18:43:39 +0300

    Extend types

--- pyrus/models/requests.py
+++ pyrus/models/requests.py
@@ -128,7 +128,7 @@ class TaskCommentRequest(object):
             approvals_added (:obj:`list` of :obj:`list` of :obj:`models.entities.Person`, optional) List of approval steps to add to the task
             approvals_removed (:obj:`list` of :obj:`list` of :obj:`models.entities.Person`, optional) List of approval steps to remove from the task
             approvals_rerequested (:obj:`list` of :obj:`list` of :obj:`models.entities.Person`, optional) List of approval steps to rerequest for the task
-            channel (:obj:`str`) External channel to send notification (email)
+            channel (:obj:`str`) External channel to send notification (email,telegram,web,facebook,vk,viber,mobile_app,web_widget,moy_sklad,zadarma,amo_crm)
     """
 
     def __init__(self, text=None, approval_choice=None, approval_steps=None, action=None,
@@ -306,8 +306,8 @@ class TaskCommentRequest(object):
         if channel:
             if not isinstance(channel, str):
                 raise TypeError('channel must be an instance of str')
-            if channel != 'email':
-                raise TypeError('channel must be equal to email')
+			if channel not in ['email', 'telegram', 'web', 'facebook', 'vk', 'viber', 'mobile_app', 'web_widget', 'moy_sklad', 'zadarma', 'amo_crm']:
+                raise TypeError('channel must be correct')
             self.channel = entities.Channel(type=channel)
         if spent_minutes:
             if not isinstance(spent_minutes, int):
